4a0f1e8db2e67cf64966df6f3535a6a1c9c455e6
Bart <bartkof91@gmail.com>
2019-05-08 23:14:35 +0200

    Basic add, remove, clear logic added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,16 +7,48 @@ import Header from "components/Header";
 import Button from "components/Button";
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [1, 2, 3]
+    };
+  }
+
+  handleClear = () => {
+    this.setState({ items: [] });
+  };
+
+  handleAdd = () => {
+    this.setState(prev => {
+      
+        return {
+          items: [...prev.items, "new"]
+        }
+    });
+  };
+
+  handleRemove = () => {
+    this.setState(state => {
+      const tasks = state.items.filter((elem, i, array) => {
+        return i !== array.length - 1;
+      });
+
+      return {
+        items: tasks
+      };
+    });
+  };
+
   render() {
     return (
       <>
         <div className="c-container-full c-app__header">
           <Header />
           <div className="o-flex">
-            <Button name="Add" />
-            <Button name="Remove" />
-            <Button name="Clear" />
+            <Button fn={this.handleAdd} name="Add" />
+            <Button fn={this.handleRemove} name="Remove" />
+            <Button fn={this.handleClear} name="Clear" />
           </div>
         </div>
         <div className="c-container" />
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,9 +3,12 @@ import React from 'react';
 import './Button.scss';
 
 function Button(props) {
+
+  const {name, fn} = props
+  
   return (
-    <div className="c-button">
-      <p>{props.name}</p>
+    <div onClick={fn} className="c-button">
+      <p>{name}</p>
     </div>
   );
 }
