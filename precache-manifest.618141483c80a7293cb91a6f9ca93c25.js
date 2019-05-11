5a692693323de168cd035cee5bd38a06891a1806
Bart <bartkof91@gmail.com>
2019-05-11 18:43:28 +0200

    Updates

---
+++ precache-manifest.618141483c80a7293cb91a6f9ca93c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0166aca6bcb27bb9757e29ae76dca50",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "d3ae7be0fc99a2c142fe",
+    "url": "/toDoApp/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "19ce55e46e8575810247",
+    "url": "/toDoApp/static/css/main.7f26cd61.chunk.css"
+  },
+  {
+    "revision": "d3ae7be0fc99a2c142fe",
+    "url": "/toDoApp/static/js/2.ba74f4c4.chunk.js"
+  },
+  {
+    "revision": "19ce55e46e8575810247",
+    "url": "/toDoApp/static/js/main.5c89f7c9.chunk.js"
+  },
+  {
+    "revision": "deec5c49507e9aa3d8dd",
+    "url": "/toDoApp/static/js/runtime~main.06834d9d.js"
+  }
+]);
